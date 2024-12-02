f5707168cea15df2ba46f380b5f3b75ef0d862f7
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2024-12-02 14:58:39 +0300

    Override toString() method in Potion class to show both HP-restoring and damage-increasing effects

--- src/Potion.java
+++ src/Potion.java
@@ -9,4 +9,9 @@ public class Potion extends HealthRestorer {
     public int getDamageIncrease() {
         return this.damageIncrease;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s (restores %d HP, increases power: %d)", name, restoreAmount, damageIncrease);
+    }
 }
